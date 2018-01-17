a1dc722e89b8a092c19e16e123a27f5132b956bc
lsolondz <lsolondz@URB218-32.seasad.wustl.edu>
2018-01-17 17:18:25 -0600

    heartbeat w/light

--- heartbeat/heartbeat.ino
+++ heartbeat/heartbeat.ino
@@ -6,13 +6,16 @@
 
 void setup() {
   Serial.begin(9600);
-  
+  pinMode(13, OUTPUT);
   
 }
 
 void loop() {
-   
+
+   delay(500);
+   digitalWrite(13, HIGH);
    Serial.print(millis()/1000);
    Serial.println(" sec have elapsed");
-   delay (1000);
+   delay (500);
+   digitalWrite(13,LOW);
 }
