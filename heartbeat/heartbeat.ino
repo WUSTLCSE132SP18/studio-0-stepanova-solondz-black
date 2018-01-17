8b682e6ddb12f8ba6bd19ced650d1083086834c9
jessica.stepanova <jessica.stepanova@URB218-31.seasad.wustl.edu>
2018-01-17 17:16:48 -0600

    Heatbeat

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  delay(500);
+  digitalWrite(13, HIGH);
+  Serial.print(millis()/1000);
+  Serial.println(" sec have elapsed");
+  delay(500);
+  digitalWrite(13, LOW);
+  
+  
+}
