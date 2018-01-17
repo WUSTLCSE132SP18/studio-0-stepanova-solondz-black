ac2700e59efec7a9201a152e7960cb262abab157
lsolondz <lsolondz@URB218-32.seasad.wustl.edu>
2018-01-17 17:06:38 -0600

    heartbeat

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,18 @@
+/* heartbeat
+ *
+ * output heartbeat
+ *
+ */
+
+void setup() {
+  Serial.begin(9600);
+  
+  
+}
+
+void loop() {
+   
+   Serial.print(millis()/1000);
+   Serial.println(" sec have elapsed");
+   delay (1000);
+}
